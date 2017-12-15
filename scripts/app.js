45ddd154b52ad4d2e20d91b9ee121107988f0ed3
Qing qing He <qingqinghe916@hotmail.com>
2017-12-15 18:34:35 +1300

    qq music 2.0

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,20 @@
 (function(){
 
     fetch('../json/rec.json')
-    then('/json/rec.json')
-
         .then(res => res.json())
         .then(render)
 
 
 
     function render(json) {
-        let slides = json.data.slider.map(slide => {
+        renderSlider(json.data.slider)
+        renderRadios(json.data.radioList)
+        renderPlaylists(json.data.songList)
+
+    }
+
+    function renderSlider(sliders){
+        let slides = sliders.map(slide => {
             return {link: slide.linkUrl, image: slide.picUrl}
         })
 
@@ -19,16 +24,31 @@
         })
     }
 
-    // let slider = new Slider(
-    //     {
-    //        el: document.querySelector('#slider'),
-    //         slides: [
-    //             {link: '#1', image: 'images/amei.jpg'},
-    //             {link: '#2', image: 'images/dakang.jpg'},
-    //             {link: '#3', image: 'images/halin.jpg'},
-    //             {link: '#4', image: 'images/lvcheng.jpg'},
-    //             {link: '#5', image: 'images/wuyifan.jpg'}
-    //         ]
-    //     })
-    // window.slider = slider
+    function renderRadios(radios) {
+        document.querySelector('.radios .list').innerHTML = radios.map(radio =>
+        `
+         <div class="list-item" >
+              <div class="list-media">
+                   <img  src="${radio.picUrl}">
+                   <span class="icon icon_play"></span>
+              </div>
+              <div class="list-title">${radio.Ftitle}</div>
+         </div>
+        `).join('')
+    }
+
+    function renderPlaylists(playlists){
+        document.querySelector('.playlists .list').innerHTML = playlists.map(playlist =>
+            `
+         <div class="list-item" >
+              <div class="list-media">
+                   <img  src="${playlist.picUrl}">
+                   <span class="icon icon_play"></span>
+              </div>
+              <div class="list-title">${playlist.songListDesc}</div>
+         </div>
+        `).join('')
+    }
+
+
 })()
